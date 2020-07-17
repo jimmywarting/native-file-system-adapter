015608f9fbf3c1b08a63015217c0d5a2b623de24
Jimmy Wärting <jimmy@warting.se>
2020-07-17 13:51:49 +0200

    updated jsdoc

--- src/showDirectoryPicker.js
+++ src/showDirectoryPicker.js
@@ -2,8 +2,6 @@ const native = globalThis.showDirectoryPicker
 
 /**
  * @param {Object} [options]
- * @param {boolean} [options.multiple] If you want to allow more than one file
- * @param {string} [options._name] the name to fall back to when using polyfill
  * @param {boolean} [options._preferPolyfill] If you rather want to use the polyfill instead of the native
  * @returns Promise<FileSystemDirectoryHandle>
  */
@@ -14,7 +12,6 @@ async function showDirectoryPicker (options = {}) {
 
   const input = document.createElement('input')
   input.type = 'file'
-  input.multiple = !!options.multiple
   input.webkitdirectory = true
 
   return new Promise(rs => {
