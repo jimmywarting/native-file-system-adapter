905a2afc69480f1fa15e192ae5d80bc3cc245b15
Jimmy Wärting <jimmy@warting.se>
2020-07-17 23:31:20 +0200

    remove boiler code

--- src/FileSystemWritableFileStream.js
+++ src/FileSystemWritableFileStream.js
@@ -3,13 +3,6 @@
 // @ts-ignore
 import {WritableStream as _WritableStream} from 'https://cdn.jsdelivr.net/npm/web-streams-polyfill@2.1.0/dist/ponyfill.es2018.mjs'
 
-
-/**
- * @alias {WritableStream}
- */
-var F = _WritableStream
-
-
 class FileSystemWritableFileStream extends (globalThis.WritableStream || _WritableStream) {
   constructor (sink) {
     super(sink)
