f56afb328d018ca0c161bb5b325eaa8ea8ec9b0f
Jimmy Wärting <jimmy@warting.se>
2021-11-15 09:50:44 +0100

    fix: Add back types (#26)

---
+++ types/src/adapters/sandbox.d.ts
@@ -0,0 +1,39 @@
+export class FileHandle {
+    constructor(file: FileEntry, writable?: boolean);
+    file: FileEntry;
+    kind: string;
+    writable: boolean;
+    readable: boolean;
+    get name(): string;
+    isSameEntry(other: any): boolean;
+    getFile(): Promise<File>;
+    createWritable(opts: any): Promise<Sink>;
+}
+export class FolderHandle {
+    constructor(dir: DirectoryEntry, writable?: boolean);
+    dir: DirectoryEntry;
+    writable: boolean;
+    readable: boolean;
+    kind: string;
+    name: string;
+    isSameEntry(other: FolderHandle): boolean;
+    entries(): AsyncGenerator<[string, FileHandle | FolderHandle]>;
+    getDirectoryHandle(name: string, opts: {
+        create: boolean;
+    }): Promise<FolderHandle>;
+    getFileHandle(name: string, opts: {
+        create: boolean;
+    }): Promise<FileHandle>;
+    removeEntry(name: string, opts: {
+        recursive: boolean;
+    }): Promise<any>;
+}
+declare function _default(opts?: {}): Promise<any>;
+export default _default;
+declare class Sink {
+    constructor(writer: FileWriter, fileEntry: FileEntry);
+    writer: FileWriter;
+    fileEntry: FileEntry;
+    write(chunk: BlobPart | any): Promise<any>;
+    close(): Promise<any>;
+}
---
+++ types/src/showDirectoryPicker.d.ts
@@ -0,0 +1,5 @@
+export default showDirectoryPicker;
+export type FileSystemDirectoryHandle = import('./FileSystemDirectoryHandle.js').default;
+export function showDirectoryPicker(options?: {
+    _preferPolyfill?: boolean;
+}): Promise<FileSystemDirectoryHandle>;
