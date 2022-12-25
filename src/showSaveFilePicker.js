c654c61a50f4ea068ffd583952d0c3675d707f54
garsue <labs.garsue@gmail.com>
2022-12-26 02:18:44 +0900

    Rename `accepts` to `types` in `showSaveFilePicker` options

--- src/showSaveFilePicker.js
+++ src/showSaveFilePicker.js
@@ -5,7 +5,7 @@ const native = globalThis.showSaveFilePicker
 /**
  * @param {Object} [options]
  * @param {boolean} [options.excludeAcceptAllOption=false] Prevent user for selecting any
- * @param {Object[]} [options.accepts] Files you want to accept
+ * @param {Object[]} [options.types] Files you want to accept
  * @param {string} [options.suggestedName] the name to fall back to when using polyfill
  * @param {string} [options._name] the name to fall back to when using polyfill
  * @param {boolean} [options._preferPolyfill] If you rather want to use the polyfill instead of the native
