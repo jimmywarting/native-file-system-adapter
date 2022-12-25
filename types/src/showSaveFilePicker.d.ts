2ede189f246f8db375a50160908b52c01c7f5609
garsue <labs.garsue@gmail.com>
2022-12-26 02:24:52 +0900

    npm run _prepublishOnly

--- types/src/showSaveFilePicker.d.ts
+++ types/src/showSaveFilePicker.d.ts
@@ -2,7 +2,7 @@ export default showSaveFilePicker;
 export type FileSystemFileHandle = import('./FileSystemFileHandle.js').default;
 export function showSaveFilePicker(options?: {
     excludeAcceptAllOption?: boolean;
-    accepts?: any[];
+    types?: any[];
     suggestedName?: string;
     _name?: string;
     _preferPolyfill?: boolean;
