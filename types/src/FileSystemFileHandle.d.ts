41e4a721534e0f8c857d194344745efc8ee81ff1
Max Rumpf <max.rumpf1998@gmail.com>
2026-04-04 13:52:39 +0200

    Types: add 'size' to createWritable options (#78)

--- types/src/FileSystemFileHandle.d.ts
+++ types/src/FileSystemFileHandle.d.ts
@@ -3,6 +3,7 @@ export class FileSystemFileHandle extends FileSystemHandle {
     constructor(adapter: any);
     createWritable(options?: {
         keepExistingData?: boolean;
+        size?: number;
     }): Promise<FileSystemWritableFileStream>;
     getFile(): Promise<File>;
     [kAdapter]: any;
